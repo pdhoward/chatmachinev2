352a845ec3fbfb32e82b649273027e2c84b8d931
pdhoward <patrick.howard@hotmail.com>
2020-03-30 14:37:43 -0400

    fix socket ref

--- src/Components/App/App.vue
+++ src/Components/App/App.vue
@@ -114,7 +114,8 @@ export default {
         muted: this.config.app.muted,
         loading: false,
         error: null,       
-        socket : io('localhost:4000', { query: "netId=rest001" })
+        //socket : io('localhost:4000', { query: "netId=rest001" })
+        socket : io('https://chaotic.ngrok.io', { query: "netId=rest001" })
         }      
     },
     computed: {
